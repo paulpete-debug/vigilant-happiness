bb665f6e6c3d5f3c956f3a71b1950fbb9212a756
paulpete-debug <cpaulpete@gmail.com>
2025-08-28 12:19:03 +0800

    AgentRegistry.sol

---
+++ contracts/SKALE/AgentRegistry.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AgentRegistry is Ownable {
+    // [Paste the AgentRegistry code from earlier]
+}
