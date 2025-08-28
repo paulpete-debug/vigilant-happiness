b25a5b44347deec0fc799661f84ef581017b463a
paulpete-debug <cpaulpete@gmail.com>
2025-08-28 12:23:02 +0800

    Create AgentDAO.sol

---
+++ contracts/SKALE/AgentDAO.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AgentDAO is Ownable {
+    // [Paste the AgentDAO code from earlier]
+}
