36e80a9fc7f6731966e14b601f6ac015de09f4c0
paulpete-debug <cpaulpete@gmail.com>
2025-08-29 07:44:44 +0800

    Update AgentDAO.sol

--- contracts/SKALE/AgentDAO.sol
+++ contracts/SKALE/AgentDAO.sol
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AgentDAO is Ownable {
-    // [Paste the AgentDAO code from earlier]
+contract AgentDAOToken is ERC20, Ownable {
+    uint256 constant INITIAL_SUPPLY = 1000000 * 10 ** decimals(); // 1M tokens
+    uint256 public votingThreshold = 100 * 10 ** decimals(); // 100 tokens to vote
+
+    constructor() ERC20("AgentDAOToken", "AGT") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY); // Owner gets all initial tokens
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function setVotingThreshold(uint256 _newThreshold) public onlyOwner {
+        votingThreshold = _newThreshold;
+    }
+
+    function canVote(address voter) public view returns (bool) {
+        return balanceOf(voter) >= votingThreshold;
+    }
 }
